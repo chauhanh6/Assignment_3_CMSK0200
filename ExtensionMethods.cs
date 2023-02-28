d76270d557ed822798ee8e0661785a4581440a2a
Hemant-Chauhan <40966812+Hemant-Chauhan@users.noreply.github.com>
2023-02-27 20:08:12 -0700

    Add project files.

---
+++ ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_CMSK0200
+{
+    public static class ExtensionMethods
+    {
+        public static int GetWordCount( this string str )
+        { 
+            return str.Split(' ').Length;
+             
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment_3_CMSK0200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Welcome to the Advanced C# class";
+            int wordCount = myString.GetWordCount();
+            Console.WriteLine("String value: " + myString);
+            Console.WriteLine("Word Count: " + wordCount);
+        }
+    }
+}
